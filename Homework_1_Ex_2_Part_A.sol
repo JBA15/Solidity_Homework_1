c29190ddaea7c3513dffcb22e42bdc542e3f1bbf
Jean-Baptiste Astruc <jeanbaptisteastruc@gmail.com>
2025-01-29 20:55:47 +0100

    Solidity conventions

--- Homework_1_Ex_2_Part_A.sol
+++ Homework_1_Ex_2_Part_A.sol
@@ -10,15 +10,15 @@ pragma solidity ^0.8.2;
 /* We want to write a function that intakes a binary number as a string and returns a decimal integer. 
 For the sake of simplicity, let's assume that the string binary input is a positive (unsigned) binary number. */
 
-contract StringConversionToDecimal {
+contract StringConversionToDecimal{
     /// @notice Here we build a function to convert a string into a decimal integer
     /// @param binaryString The binary representation of a string
     /// @return decimalValue The integer of the converted string
-    function convertBinaryToDecimal(string calldata binaryString) external pure returns (uint256) {
+    function convertBinaryToDecimal(string calldata binaryString) external pure returns (uint256){
         bytes calldata binaryArray = bytes(binaryString);
         uint256 decimalValue = 0;
 
-        for (uint256 i = 0; i < binaryArray.length; i++) {
+        for (uint256 i = 0; i < binaryArray.length; i++){
             // We implement a sanity check to be sure that it is a binary output
             require(
                 binaryArray[i] == '0' || binaryArray[i] == '1',
